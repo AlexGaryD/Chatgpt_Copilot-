aa9ff75b5144bce7e34ed811eeaf7d673f7e1fd0
Alexis Vera S. <alxvcs93@hotmail.com>
2024-11-04 20:50:04 -0500

    cambios en el index

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.buy-button');
+    const whatsappNumber = '0999097453'; // Número de WhatsApp deseado
+
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             const product = this.getAttribute('data-product');
-            const whatsappUrl = `https://wa.me/?text=Estoy%20interesado%20en%20el%20${encodeURIComponent(product)}`;
-            window.location.href = whatsappUrl;
+            const message = `👋 Hola, estoy interesado en comprar el ${product} de Anime Store. ¿Podría darme más detalles?`;
+            const advertisement = '✨ ¡No te pierdas nuestras ofertas exclusivas! ✨';
+            const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message + ' ' + advertisement)}`;
+            
+            // Validar el número de WhatsApp
+            if (whatsappNumber.match(/^\d{10,15}$/)) {
+                window.location.href = whatsappUrl;
+            } else {
+                alert('El número de WhatsApp no es válido.');
+            }
         });
     });
 });
