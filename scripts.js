3fa377b31cae867b13a3bfa34588cf094649d7e2
Alexis Vera S. <alxvcs93@hotmail.com>
2024-11-05 11:50:02 -0500

    Hice cambios en el script para que me mande a whatsapp y la comprobación este en mi numero celular

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.buy-button');
-    const whatsappNumber = '0999097453'; // Número de WhatsApp deseado
+    const whatsappNumber = '593999097453'; // Número de WhatsApp deseado con el código de país para Ecuador
 
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             const product = this.getAttribute('data-product');
-            const message = `👋 Hola, estoy interesado en comprar la figura de ${product} de Anime Store. ¿Podría darme más detalles?`;
-            const advertisement = '✨ ¡No te pierdas nuestras ofertas exclusivas! ✨';
-            const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message + ' ' + advertisement)}`;
+            const message = `Hola,👋  estoy interesado en comprar la figura de ${product} de Anime Store. ¿Podría darme más detalles?`;
+            const advertisement = ' ¡✨No te pierdas nuestras ofertas exclusivas✨! ';
+            const fullMessage = `${message} ${advertisement}`;
+            const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(fullMessage)}`;
             
             // Validar el número de WhatsApp
-            if (whatsappNumber.match(/^\d{10,15}$/)) {
+            if (/^\d{11,15}$/.test(whatsappNumber)) {
                 window.location.href = whatsappUrl;
             } else {
                 alert('El número de WhatsApp no es válido.');
